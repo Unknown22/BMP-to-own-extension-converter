50f30796f2a4daee1f9c253cabf11cfeaec4173e
Unknown22 <marcin700@gmail.com>
2015-02-03 14:36:50 +0100

    Init files
    Needs to get path from OpenFileDialog and give it to konwerter(path)

---
+++ Konsolowy/MyForm.h
@@ -0,0 +1,177 @@
+#pragma once
+
+namespace Konsolowy {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TabControl^  tabControl1;
+	protected:
+	private: System::Windows::Forms::TabPage^  tabPage1;
+	private: System::Windows::Forms::TabPage^  tabPage2;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::TabPage^  tabPage3;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::OpenFileDialog^  openFile1;
+
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
+			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->tabPage3 = (gcnew System::Windows::Forms::TabPage());
+			this->openFile1 = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->tabControl1->SuspendLayout();
+			this->tabPage1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this->tabControl1->Controls->Add(this->tabPage1);
+			this->tabControl1->Controls->Add(this->tabPage2);
+			this->tabControl1->Controls->Add(this->tabPage3);
+			this->tabControl1->Location = System::Drawing::Point(13, 13);
+			this->tabControl1->Name = L"tabControl1";
+			this->tabControl1->SelectedIndex = 0;
+			this->tabControl1->Size = System::Drawing::Size(259, 237);
+			this->tabControl1->TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this->tabPage1->Controls->Add(this->button1);
+			this->tabPage1->Controls->Add(this->textBox1);
+			this->tabPage1->Controls->Add(this->label1);
+			this->tabPage1->Location = System::Drawing::Point(4, 22);
+			this->tabPage1->Name = L"tabPage1";
+			this->tabPage1->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage1->Size = System::Drawing::Size(251, 211);
+			this->tabPage1->TabIndex = 0;
+			this->tabPage1->Text = L"BMP -> MMSS";
+			this->tabPage1->UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(200, 24);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(45, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"...";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Enabled = false;
+			this->textBox1->Location = System::Drawing::Point(4, 24);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(189, 20);
+			this->textBox1->TabIndex = 1;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(4, 7);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(67, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Wybierz plik:";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// tabPage2
+			// 
+			this->tabPage2->Location = System::Drawing::Point(4, 22);
+			this->tabPage2->Name = L"tabPage2";
+			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage2->Size = System::Drawing::Size(251, 211);
+			this->tabPage2->TabIndex = 1;
+			this->tabPage2->Text = L"MMSS -> BMP";
+			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// tabPage3
+			// 
+			this->tabPage3->Location = System::Drawing::Point(4, 22);
+			this->tabPage3->Name = L"tabPage3";
+			this->tabPage3->Size = System::Drawing::Size(251, 211);
+			this->tabPage3->TabIndex = 2;
+			this->tabPage3->Text = L"Informacje";
+			this->tabPage3->UseVisualStyleBackColor = true;
+			// 
+			// openFile1
+			// 
+			this->openFile1->FileName = L"Wybierz plik...";
+			this->openFile1->Filter = L"Mapa bitowa 6-bitowa (*.bmp)|*.bmp";
+			this->openFile1->FileOk += gcnew System::ComponentModel::CancelEventHandler(this, &MyForm::openFileDialog1_FileOk);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 262);
+			this->Controls->Add(this->tabControl1);
+			this->Name = L"MyForm";
+			this->Text = L"Konwerter BMP <-> MMSS";
+			this->tabControl1->ResumeLayout(false);
+			this->tabPage1->ResumeLayout(false);
+			this->tabPage1->PerformLayout();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+}
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+				 openFile1->ShowDialog();
+				 textBox1->Text = openFile1->FileName;
+	}
+private: System::Void openFileDialog1_FileOk(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e) {
+}
+};
+}
---
+++ Konsolowy/Source.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+
+using namespace std;
+
+#pragma pack(2)
+
+struct RGBPixel
+{
+	int Red;
+	int Green;
+	int Blue;
+};
+
+struct BMPInfoHeader
+{
+	unsigned short BMPType;			// 0x42D2
+	unsigned int FileSize;			// Size of the file
+	unsigned short Reserved1;		// Reserved
+	unsigned short Reserved2;		// Reserved
+	unsigned int OffBits;			// Offset to bitmap data
+	unsigned int HeaderSize;		// Size of Header
+	int Width;						// Width of image
+	int Height;						// Height of image
+	unsigned short Planes;			// Number of color planes
+	unsigned short BitsPerPxl;		// Number of bits per pixel
+	unsigned int Compression;		// Type of compression
+	unsigned int ImageSize;			// Size of image
+	int XPixels;					// Pixels per meter in x axis
+	int YPixels;					// Pixels per meter in y axis
+	unsigned int ColorsUsed;		// Number of colors used
+	unsigned int ColorsImportant;	// Number of important colors
+};
+#pragma pack()
+
+FILE* f;
+unsigned char* Pixels;
+BMPInfoHeader bih;
+RGBPixel ColorTable[64];
+
+
+void ReadBMP(char* path)
+{
+	f = fopen(path, "rb");
+	if (f != NULL)
+	{
+		fread(&bih, sizeof(BMPInfoHeader), 1, f);
+		if (bih.BMPType != 19778)
+		{
+			fclose(f);
+			return;
+		}
+
+		unsigned int mallocSize = bih.Height * bih.Width * 3;
+		Pixels = (unsigned char *)malloc(mallocSize * sizeof(unsigned char));
+		unsigned char pad[4] = { 0 };
+		unsigned int padding = (4 - ((bih.Width * 3) % 4)) % 4;
+		fseek(f, bih.OffBits, SEEK_SET);
+		for (unsigned int i = 0; i < bih.Height; ++i)
+		{
+			fread(Pixels + i * bih.Width * 3, (size_t)1, (size_t)bih.Width * 3, f);
+			fread(&pad, 1, padding, f);
+		}
+		cout << "Test: wczytano" << endl;
+	}
+	fclose(f);
+}
+
+void SaveFile(int t)
+{
+	int i;
+	ofstream fout("image.mmss");
+	if (t < 10)
+	{
+		fout << "0" << t;
+	}
+	else
+	{
+		fout << t;
+	}
+	for (i = 0; i < t; i++)
+	{
+		if (ColorTable[i].Red < 100)
+		{
+			fout << "0";
+			if (ColorTable[i].Red < 10)
+				fout << "0";
+		}
+		fout << ColorTable[i].Red;
+		if (ColorTable[i].Green < 100)
+		{
+			fout << "0";
+			if (ColorTable[i].Green < 10)
+				fout << "0";
+		}
+		fout << ColorTable[i].Green;
+		if (ColorTable[i].Blue < 100)
+		{
+			fout << "0";
+			if (ColorTable[i].Blue < 10)
+				fout << "0";
+		}
+		fout << ColorTable[i].Blue;
+	}
+	for (i = 0; i < bih.Height*bih.Width * 3; i += 3)
+	{
+		//fout << (int)Pixels[i + 2] << (int)Pixels[i + 1] << (int)Pixels[i];
+		for (int x = 0; x < t; x++)
+		{
+			if ((int)Pixels[i + 2] == ColorTable[x].Red &&
+				(int)Pixels[i + 1] == ColorTable[x].Green &&
+				(int)Pixels[i] == ColorTable[x].Blue)
+			{
+				if (t < 10)
+				{
+					fout << x;
+				}
+				else
+				{
+					if (x < 10)
+					{
+						fout << "0";
+					}
+					fout << x;
+				}
+			}
+		}
+	}
+}
+
+int MakeColorTable()
+{
+	RGBPixel CheckColor;
+	int t = 0; //indeks w tabeli kolorów
+	int m = 0; //okresla czy kolor byl juz w tablicy, 0 - nie byl, 1 - byl
+	for (int i = 0; i < bih.Height*bih.Width * 3; i += 3)
+	{
+		m = 0;
+		CheckColor.Red = (int)Pixels[i + 2];
+		CheckColor.Green = (int)Pixels[i + 1];
+		CheckColor.Blue = (int)Pixels[i];
+		for (int x = 0; x < t; x++)
+		{
+			if (ColorTable[x].Red == CheckColor.Red &&
+				ColorTable[x].Green == CheckColor.Green &&
+				ColorTable[x].Blue == CheckColor.Blue)
+			{
+				m = 1;
+			}
+		}
+		if (m != 1)
+		{
+			ColorTable[t].Red = CheckColor.Red;
+			ColorTable[t].Green = CheckColor.Green;
+			ColorTable[t].Blue = CheckColor.Blue;
+			t++;
+		}
+	}
+	for (int i = 0; i < t; i++)
+	{
+		cout << i << ". " << ColorTable[i].Red << ":" << ColorTable[i].Green << ":" << ColorTable[i].Blue << endl;
+	}
+	return t;
+}
+
+int konwerter(char* path)
+{
+	ReadBMP(path);
+
+	for (int x = 0; x < bih.Height*bih.Width * 3; x += 3)
+	{
+		//cout << (int)Pixels[x + 2] << ":" << (int)Pixels[x + 1] << ":" << (int)Pixels[x] << " ";
+	}
+	int t = MakeColorTable();
+	SaveFile(t);
+
+	cout << endl;
+	return 0;
+}
